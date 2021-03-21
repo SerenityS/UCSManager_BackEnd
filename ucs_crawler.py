249852ef0b4e16efe5a81477944de87a41291710
HeeRyun, Jin <jins4218@gmail.com>
2021-03-22 01:25:48 +0900

    ucs_crawler: Add songTitle_ko, songArtist_ko

--- ucs_crawler.py
+++ ucs_crawler.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 from bs4 import BeautifulSoup
@@ -5,6 +6,9 @@ from bs4 import BeautifulSoup
 ucsUrl = 'http://www.piugame.com/bbs/board.php?bo_table=ucs&page='
 
 ucsList = []
+
+with open('UCSSongData.json', encoding='utf-8') as f:
+    songDatas = json.load(f)
 
 
 def getTotalPage():
@@ -31,9 +35,15 @@ def getUcsList(page):
     ucs_list = ucs_list.find_all('tr')
 
     for ucs_data in ucs_list:
+        song_no = ucs_data.find("img")['src'].replace("/piu.songimg/", "").replace(".png", "")
+        for songData in songDatas:
+            if song_no == songData['songNo']:
+                song_title_ko = songData['songTitle_ko']
+                song_title_en = songData['songTitle_en']
+                song_artist_ko = songData['songArtist_ko']
+                song_artist_en = songData['songArtist_en']
+
         ucs_no = ucs_data.find(class_="btnaddslot_ucs btnAddtoUCSSLOT")['data-ucs_id'].strip()
-        song_title = ucs_data.find(class_="share_song_title").text.strip()
-        song_artist = ucs_data.find(class_="share_artist").text.strip()
         ucs_lv = ucs_data.find(class_="share_level").find("span")['class']
         if "single" in ucs_lv[0]:
             ucs_lv = "S" + ucs_lv[1][4:]
@@ -47,7 +57,7 @@ def getUcsList(page):
             ucs_lv = "CO-OPx" + ucs_lv[1][5:]
         step_maker = ucs_data.find(class_="share_stepmaker").text.strip()
 
-        ucsList.append((int(ucs_no), song_title, song_artist, ucs_lv, step_maker))
+        ucsList.append((int(ucs_no), song_title_ko, song_title_en, song_artist_ko, song_artist_en, ucs_lv, step_maker))
 
 
 def runCrawler(mode):
