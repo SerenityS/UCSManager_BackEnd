76333f5a7a7a294f82ba78557f97b57cfbbdb503
HeeRyun, Jin <jins4218@gmail.com>
2021-03-22 12:47:10 +0900

    UCSFlask: Add Flask Script

---
+++ UCSFlask.py
@@ -0,0 +1,39 @@
+import json
+
+from flask import Flask, request
+from ucstosqlite import UcsSql
+
+app = Flask(__name__)
+
+
+@app.route('/getucs')
+def getUcs():
+    param = request.args.to_dict()
+    if len(param) == 0:
+        return 'Error! No parameter found.'
+
+    if param['songTitle'] != '':
+        song_title = param['songTitle']
+    else:
+        song_title = 'None'
+    if param['songLv'] != '':
+        song_lv = param['songLv']
+    else:
+        song_lv = 'None'
+    if param['stepArtist'] != '':
+        step_maker = param['stepArtist']
+    else:
+        step_maker = 'None'
+
+    result = UcsSql().findFromSql(song_title, song_lv, step_maker)
+
+    response = app.response_class(
+        response=json.dumps(result, ensure_ascii=False),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
