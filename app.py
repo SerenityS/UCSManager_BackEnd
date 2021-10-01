94421ab1d35d4768a53685e3603228960d85b193
HeeRyun, Jin <jins4218@gmail.com>
2021-10-02 00:36:28 +0900

    Support UCS Contest Pack

--- app.py
+++ app.py
@@ -26,6 +26,28 @@ def getUcs():
     )
     return response
 
+@app.route('/getpack')
+def getPack():
+    param = request.args.to_dict()
+    if len(param) == 0:
+        return 'Error! No parameter found.'
+
+    with open('UCSContestPackData.json', encoding='utf-8') as f:
+        packData = json.load(f)
+
+    try:
+        response = app.response_class(
+            response=json.dumps(packData[param['pack']], ensure_ascii=False),
+            status=200,
+            mimetype='application/json'
+        )
+    except:
+        response = app.response_class(
+            response=json.dumps([], ensure_ascii=False),
+            status=200,
+            mimetype='application/json'        )
+    return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
